6a51ea62b9058eea85766b3d38420f770ee15326
PolleEdouard <bab.lafarge@gmail.com>
2025-02-05 13:55:37 +0100

    git init

---
+++ server/manifest.js
@@ -0,0 +1,69 @@
+'use strict';
+
+const Dotenv = require('dotenv');
+const Confidence = require('@hapipal/confidence');
+const Toys = require('@hapipal/toys');
+const Schwifty = require('@hapipal/schwifty');
+
+// Pull .env into process.env
+Dotenv.config({ path: `${__dirname}/.env` });
+
+// Glue manifest as a confidence store
+module.exports = new Confidence.Store({
+    server: {
+        host: 'localhost',
+        port: {
+            $param: 'PORT',
+            $coerce: 'number',
+            $default: 3000
+        },
+        debug: {
+            $filter: 'NODE_ENV',
+            $default: {
+                log: ['error', 'start'],
+                request: ['error']
+            },
+            production: {
+                request: ['implementation']
+            }
+        }
+    },
+    register: {
+        plugins: [
+            {
+                plugin: '../lib', // Main plugin
+                options: {}
+            },
+            {
+                plugin: '@hapipal/schwifty',
+                options: {
+                    $filter: 'NODE_ENV',
+                    $default: {},
+                    $base: {
+                        migrateOnStart: true,
+                        knex: {
+                            client: 'sqlite3',
+                            useNullAsDefault: true,     // Suggested for sqlite3
+                            connection: {
+                                filename: ':memory:'
+                            },
+                            migrations: {
+                                stub: Schwifty.migrationsStubPath
+                            }
+                        }
+                    },
+                    production: {
+                        migrateOnStart: false
+                    }
+                }
+            },
+            {
+                plugin: {
+                    $filter: 'NODE_ENV',
+                    $default: '@hapipal/hpal-debug',
+                    production: Toys.noop
+                }
+            }
+        ]
+    }
+});
